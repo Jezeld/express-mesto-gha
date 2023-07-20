db85c79b71be53c9de2b259d7251f0ee24667e25
Evgenia <jezeld@mail.ru>
2023-07-20 14:56:40 +0300

    ПР15деплой

--- app.js
+++ app.js
@@ -2,20 +2,32 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const { errors } = require('celebrate');
+// const cors = require('./middlewares/cors');
 const errorProcessor = require('./middlewares/errorprocessor');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
+// const { requestLogger, errorLogger } = require('./middlewares/logger');
 const NotFoundError = require('./errors/notfound');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
-mongoose.connect('mongodb://127.0.0.1/mestodb');
+
+mongoose
+  .connect(URL)
+  .then(() => {
+    console.log('БД успешно подключена');
+  })
+  .catch((err) => {
+    console.log(`Ошибка подключения: ${err}`);
+  });
+
+// app.use(cors);
+
 app.use(express.json());
 
-app.use(requestLogger); // подключаем логгер запросов
+// app.use(requestLogger); // подключаем логгер запросов
 
 app.use(require('./routes'));
 
-app.use(errorLogger); // подключаем логгер ошибок
+// app.use(errorLogger); // подключаем логгер ошибок
 
 app.use(errors());
 
